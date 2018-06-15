98f147cc710e3a227a07844ab66faf903d51cc6f
Ardalan Samimi <ardalan.samimi@gmail.com>
2018-06-15 11:56:29 +0200

    Update formula

--- Formula/hazel.rb
+++ Formula/hazel.rb
@@ -4,8 +4,9 @@ class Hazel < Formula
   url "https://github.com/pkrll/Hazel/archive/1.0.1.tar.gz"
   version "1.0.1"
   sha256 "20034fd106c2cf6ac7cef0ee2da0a5c1ead71d2958556b8590bb694da489e27e"
+  head "https://github.com/pkrll/Hazel.git"
 
   def install
-
+    system "make", "install"
   end
 end

457f69e68a3dd878198b8b8b95ba74162ca7cf80
Ardalan Samimi <ardalan.samimi@gmail.com>
2018-06-16 17:45:07 +0200

    Update Hazel formula

--- Formula/hazel.rb
+++ Formula/hazel.rb
@@ -1,9 +1,9 @@
 class Hazel < Formula
   desc "A scaffolding tool that'll help you kickstart new projects with ease."
   homepage "https://github.com/pkrll/hazel"
-  url "https://github.com/pkrll/Hazel/archive/1.0.2.tar.gz"
-  version "1.0.2"
-  sha256 "57510185b8b8ed40640ed9f1e1e582fe09146e857f0d4987696d811f601aa52c"
+  url "https://github.com/pkrll/Hazel/archive/1.0.3.tar.gz"
+  version "1.0.3"
+  sha256 "eecbeff8d3fe0f8039e5b4056ad391e9ebc4a1a73dbfae799aaf25f289854703"
   head "https://github.com/pkrll/Hazel.git"
 
   def install
@@ -11,11 +11,8 @@ class Hazel < Formula
     bin.install ".build/release/hazel"
     system "mkdir -p /Users/`whoami`/.hazel"
     system "cd .assets && cp -r templates /Users/`whoami`/.hazel"
-    system "cd .assets/scripts && cp -r completion /Users/`whoami`/.hazel"
-  end
-
-  def uninstall
-    system "echo haha"
+    bash_completion.install ".assets/scripts/completion/bash/hazel"
+    zsh_completion.install ".assets/scripts/completion/zsh/_hazel"
   end
 
 end

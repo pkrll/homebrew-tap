a1345b3a474fef3bc639a5bef65f7c60e6d8d900
Ardalan Samimi <ardalan.samimi@gmail.com>
2019-03-16 20:48:54 +0100

    Update formula

--- Formula/hazel.rb
+++ Formula/hazel.rb
@@ -1,9 +1,9 @@
 class Hazel < Formula
   desc "A scaffolding tool that'll help you kickstart new projects with ease."
   homepage "https://github.com/pkrll/hazel"
-  url "https://github.com/pkrll/Hazel/archive/1.1.0.tar.gz"
-  version "1.1.0"
-  sha256 "9946ac4bb6cec9ec7336d708226cbbeec606c0dd1a08e3976212e1969453812e"
+  url "https://github.com/pkrll/Hazel/archive/1.1.1.tar.gz"
+  version "1.1.1"
+  sha256 "4c8a6c6844b698ce2586101ee59aab4de97607be978049515a845982ee831a9d"
   head "https://github.com/pkrll/Hazel.git"
 
   def install
